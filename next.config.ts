7babf62c7f7b932471cb19e1437f7193a1846f2f
zyndor1548 <a.s.aromal2006@gmail.com>
2026-08-19 21:58:46 +0530

    link tree fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/**': ['./src/content/**/*'],
+  },
   images: {
     loader: 'custom',
     loaderFile: './cloudinary-loader.js',
